02d6600624dbddb55354eb0f4091150848eefa72
jjub0217 <noonssub1004@gmail.com>
2020-08-18 12:35:14 +0900

    object

--- doit_practice/object/퀴즈3.js
+++ doit_practice/object/퀴즈3.js
@@ -1,2 +1,23 @@
-let today = new Date('2019-06-14');
-console.log(today);
+// let day = new Date();
+// // console.log(day); // 2020-08-17T12:31:11.357Z
+
+// // console.log(day.getFullYear()); // 2020 <- day 의 연도를 가져오는 함수
+// // console.log(day.getMonth() + 1 + '월') ; // 7 <- day 의 월 을 가져오는 함수 : 주의! 0 은 1월 이고 1은 2월... 이므로 1을 더해줘야 진짜 월이 나온다.
+// // console.log(day.getDate()); // 17 <- day 의 일을 가져오는 함수
+// // console.log(day.getDay()); // 1 <- 요일을 가져오는 함수 : 주의! 0 은 일요일이고 1은 월요일이고... 6은 토요일이다.
+// // console.log(day.getTime()); // 1597667765300 <- 1970년 1월 1일 자정 이후의 시간을 밀리초로 표시
+// // console.log(day.getHours()); // 21 <- day 의 시 를 가져오는 함수 
+// // console.log(day.getMinutes()); // 37 <- day 의 분을 가져오는 함수
+// // console.log(day.getSeconds()); // 50 <- day 의 초를 가져오는 함수
+// // console.log(day.getMilliseconds()); // 98 <- day 의 밀리초를 가져오는 함수
+
+// day.setDate(day.getDate()+ 50) ;
+// console.log(day.setDate(day.getDate()+ 50)); // 1606309397062 <- 결과값이 밀리초로 표시된다.
+// console.log(day); // 2020=10-06T13:01:58
+
+
+
+let year = 3; // <- 변수선언
+console.log(year);
+year = 2; // <- 값의 재할당
+console.log(year);   

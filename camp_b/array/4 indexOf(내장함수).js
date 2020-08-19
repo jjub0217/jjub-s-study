b1562b627fd3109c07fa95829208b3e1e8b09a3d
jjub0217 <noonssub1004@gmail.com>
2020-08-19 21:29:55 +0900

    array

---
+++ camp_b/array/4 indexOf(내장함수).js
@@ -0,0 +1,11 @@
+// 배열에서 원하는 항목이 어디에 있는지 알려주는 함수
+
+// 과연, 토르가 몇번째 있는 요소일까요?
+const superHeroes = [ '아이언맨', '캡틴 아메리카', '토르', '닥터스트레인지'];
+
+const index1 = superHeroes.indexOf('토르')
+console.log(index1); // 2 <- 2번째 인덱스에 '토르' 라는 요소가 존재한다.
+
+const index2 = superHeroes.indexOf('닥터스트레인지')
+console.log(index2); // 3 <- 3번째 인덱스에 '닥터스트레인지' 라는 요소가 존재한다
+
---
+++ camp_b/array/5 findIndex(내장함수) copy.js
@@ -0,0 +1,2 @@
+// < findIndex 내장함수 >
+//      - indexOf 함수와 비슷하다

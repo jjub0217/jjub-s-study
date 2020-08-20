8e9f37b64e3ac6d2ee14576f3cb29617ff53f2bb
jjub0217 <noonssub1004@gmail.com>
2020-08-20 13:59:22 +0900

    array

--- camp_b/array/4 indexOf(내장함수).js
+++ camp_b/array/4 indexOf(내장함수).js
@@ -1,9 +1,13 @@
-// 배열에서 원하는 항목이 어디에 있는지 알려주는 함수
+// indexOf 
+//      - indexOf 안에 들어가는 매개변수는 요소의 값이 된다.  
+//      - 배열에서 원하는 항목이 몇번째에 있는지 알려주는 함수
+//      - 특정값과 일치하는 요소를 찾을때 사용한다. 
+
 
 // 과연, 토르가 몇번째 있는 요소일까요?
 const superHeroes = [ '아이언맨', '캡틴 아메리카', '토르', '닥터스트레인지'];
 
-const index1 = superHeroes.indexOf('토르')
+const index1 = superHeroes.indexOf('토르') // <- indexOf 안에 들어가는 매개변수는 요소의 값이 된다.  
 console.log(index1); // 2 <- 2번째 인덱스에 '토르' 라는 요소가 존재한다.
 
 const index2 = superHeroes.indexOf('닥터스트레인지')
---
+++ camp_b/array/5 findIndex,find(내장함수).js
@@ -0,0 +1,57 @@
+// < findIndex 내장함수 >
+//      - indexOf 함수와 비슷하다
+//      - 배열 내부의 요소가 '문자열' 이 아닌 다른 데이터 타입의 값의 인덱스를 찾거나
+//        어떤 조건으로 찾을때 사용
+//      - findIndex 안에 들어가는 매개변수는 함수가 들어간다.
+//      - 특정 조건을 확인해서 그 조건이 일치한다면 일치하는 요소가 몇번째인지 알려준다.
+//      - findIndex 내부에 함수를 넣어서 특정값을 특정조건으로 찾아서 그게 
+//        몇번째인지 알려준다. 
+
+
+
+
+const todos = [
+    {
+        id: 1,
+        text: '자바스크립트 입문',
+        done: true
+    },
+    {
+        id: 2,
+        text: '함수배우기',
+        done: true
+    },
+    {
+        id: 3,
+        text: '객체와 배열 배우기',
+        done: true
+    },
+    {
+        id: 4,
+        text: '배열 내장함수 배우기',
+        done: false
+    }
+]
+
+// id가 3인것을 찾는다면?
+
+// const index = todos.indexOf(3);
+// console.log(index); // -1  <- 일치하는게 없다는 의미의 -1 이 나온다. 
+// //  <- indexOf 는 이때는 사용할수 없다. 
+
+const index = todos.findIndex(todo => todo.id === 3) // <- findIndex 안에 들어가는 매개변수는 함수가 들어간다.
+                                //  특정 조건을 확인해서 그 조건이 일치한다면 일치하는 요소가 몇번째인지 알려준다.
+console.log(index); // 2 <- 인덱스 2 번째 요소의 id가 2 이다 
+
+/////////////////////////////////////////////////////////////////
+
+
+
+
+// find 내장함수
+const index2 = todos.find(todo => todo.id === 3);
+console.log(index2); // { id: 3, text: '객체와 배열 배우기', done: true }
+                    // <- id 가 3 요소 전체를 출력해라. (찾은 특정 값 자체를 출력)
+const index3 = todos.find(todo => todo.done === false);
+console.log(index3); // { id: 4, text: '배열 내장함수 배우기', done: false }
+                    // <- done 이라는 프로퍼티의 값이 false 인 요소 전체를 출력해라. 

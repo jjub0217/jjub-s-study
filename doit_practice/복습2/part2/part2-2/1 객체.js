22c70f7fc1b9b70c7f31a62ad1b220a1342ffc61
jjub0217 <noonssub1004@gmail.com>
2020-08-29 18:50:41 +0900

    object

--- doit_practice/복습2/part2/part2-2/1 객체.js
+++
@@ -1,8 +0,0 @@
-let string = prompt('문자를 입력해주세요');
-
-string.split('');
-console.log();
-
-function reverseString(string){
-
-}
---
+++ doit_practice/복습2/part2/part2-2/2 프로퍼티.js
@@ -0,0 +1,15 @@
+/* 프로퍼티
+        - 프로퍼티 키 : 객체에서 값을 담고 있는 정보
+        - 프로퍼티 값 : 프로퍼티 키의 값*/
+//      - 예시
+        const fds17 = {
+            name: '강주현',
+            gender: 'female',
+            age: 33
+        }
+//          name / gender / age  => 프로퍼티 키
+//          '강주현' / 'female' / 33  => 프로퍼티 값                    
+            console.log(fds17.name); // 강주현
+            console.log(fds17.gender); // female
+            console.log(fds17.age); // 33
+

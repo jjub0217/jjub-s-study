0f6140d1c6d67f185baad0f66906bb17314c5a41
jjub0217 <noonssub1004@gmail.com>
2020-08-30 16:02:05 +0900

    object

---
+++ doit_practice/복습2/part2/part2-2/3-1 Math객체 .js
@@ -0,0 +1,27 @@
+// Math 객체
+//      - 인스턴스를 생성하지 않는다.
+
+// let number = -Math.PI; 
+// console.log(number); // 3.141592653589793
+
+// let result = Math.abs(number);
+// console.log(result)
+
+let num1 = '';
+console.log(typeof num1); // string
+console.log(num1 === true); // false
+
+let num2 = Math.pow(2,2)
+console.log(num2); // 4
+
+let num3 = Math.pow(3,2)
+console.log(num3); // 9
+
+let num4 = Math.pow(2, -1);
+console.log(num4);  // 0.5
+
+let num5 = Math.max(11,34,36,45,41,47,74,4,7,26,73,76,73,22,3,12,72)
+console.log(num5); // 76
+
+let num6 = Math.min(11,34,36,45,41,47,74,4,7,26,73,76,73,22,3,12,72)
+console.log(num6); // 3
---
+++ doit_practice/복습2/part2/part2-2/3-2 practice.js
@@ -0,0 +1,18 @@
+let num1 = Math.random()*100 +1 ;
+let num2 = Math.random()*100 +1 ;
+let num3 = Math.random()*100 +1 ;
+let num4 = Math.random()*100 +1 ;
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+
+let num5 = Math.floor(Math.random()*100 +1) ;
+let num6 = Math.floor(Math.random()*100 +1) ;
+let num7 = Math.floor(Math.random()*100 +1) ;
+let num8 = Math.floor(Math.random()*100 +1) ;
+console.log(num5);
+console.log(num6);
+console.log(num7);
+console.log(num8);

ad54e7dcbd24faf40854578dc20dcfa4a52f7519
jjub0217 <noonssub1004@gmail.com>
2020-10-06 18:41:30 +0900

    arrow function

--- camp_b/복습2/part2/part2-1/6-2 함수.js
+++ camp_b/복습2/part2/part2-1/6-2 함수.js
@@ -1,7 +1,8 @@
 // 화살표함수: 화살표함수에서 가리키는 this 는 일반 함수에서 가리키는 this 와 다르다.
 
 
-const add = (a, b) => {
+const add = (a, b) => { // ( ) 안에 있는 것들이 매개변수이다.
+                        // { } 안에 있는 것들은 함수 몸체이다
     return a + b
 }
 const sum = add(1, 2)

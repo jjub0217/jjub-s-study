d93364d9dddb3dac88c46465630f081936ed5b6a
jjub0217 <noonssub1004@gmail.com>
2020-10-06 18:44:19 +0900

    control statment

--- poiema/복습2/for1.js
+++ poiema/복습2/for1.js
@@ -1,7 +1,7 @@
 var str = 'aaab'
-console.log(str.length);
+// console.log(str.length);
 
-// 문자 a 의 개수를 구해라.
+    // 문자 a 의 개수를 구해라.
 let count = 0
 for(let i = 0; i < str.length; i++){
     if(str[i] === 'a'){

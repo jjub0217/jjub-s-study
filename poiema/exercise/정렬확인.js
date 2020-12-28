9dd28e45d38b823b21d67b7978a9741b5debcad0
jjub0217 <noonssub1004@gmail.com>
2020-12-28 16:07:48 +0900

    isSort

---
+++ poiema/exercise/정렬확인.js
@@ -0,0 +1,15 @@
+function isSorted(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i + 1], array[i]); // 2 1 , 3 2 , 4 3 , 5 4 
+        if (array[i + 1] < array[i]) {
+            return false
+            // console.log(false);
+        }
+    }
+    return true
+    // console.log(true);
+}
+
+// console.log(isSorted([1, 2, 3, 4, 5])); // true
+// isSorted([1, 2, 3, 4, 5]); // true
+console.log(isSorted([2, 3, 4, 1, 5])); // false
